8d9b18ffd703e28c92228067d2d0d680f446dae5
Romik1999 <usankin.roma@gmail.com>
2023-02-04 00:38:07 +0400

    finished home screen

--- Navigate.js
+++ Navigate.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { SafeAreaView, Image, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from "react-native-screens/native-stack";
-import { createStackNavigator } from '@react-navigation/stack';
-import {HomeScreen} from "./ui/screens/HomeScreen";
-import {LearnScreen} from "./ui/screens/LearnScreen";
-import {SettingsScreen} from "./ui/screens/SettingsScreen";
-import {TaskScreen} from "./ui/screens/TaskScreen";
-import {ResultScreen} from "./ui/screens/ResultScreen";
-import {TestScreen} from "./ui/screens/TestScreen";
-import settings from './assets/images/settings.png';
+import React from "react";
+import { SafeAreaView, Image, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "react-native-screens/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen } from "./ui/screens/HomeScreen";
+import { LearnScreen } from "./ui/screens/LearnScreen";
+import { SettingsScreen } from "./ui/screens/SettingsScreen";
+import { TaskScreen } from "./ui/screens/TaskScreen";
+import { ResultScreen } from "./ui/screens/ResultScreen";
+import { TestScreen } from "./ui/screens/TestScreen";
+import settings from "./assets/images/settings.png";
+import settingsPink from "./assets/images/settingsPink.png";
+import { COLORS } from "./assets/style";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -19,30 +21,33 @@ export const Navigate = () => {
   const TestBlock = () => {
     return (
       <Stack.Navigator>
-        <Stack.Screen name="TestScreen" component={TestScreen} />
-        <Stack.Screen name="TaskScreen" component={TaskScreen} />
-        <Stack.Screen name="ResultScreen" component={ResultScreen} />
+        <Stack.Screen name="TestScreen" component={TestScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="TaskScreen" component={TaskScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ResultScreen" component={ResultScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     );
-  }
+  };
 
   return (
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Feed"
         screenOptions={{
-          tabBarActiveTintColor: '#e91e63',
+          tabBarActiveTintColor: "#e91e63",
+          headerShown: false,
+          tabBarStyle: { position: "absolute", backgroundColor: COLORS.green },
+
         }}
       >
         <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
+            tabBarLabel: "",
+            tabBarIcon: ({ focused, color, size }) => (
               <Image
                 style={styles.logo}
-                source={settings}
+                source={focused ? settingsPink : settings}
               />
             ),
           }}
@@ -51,11 +56,11 @@ export const Navigate = () => {
           name="Learn"
           component={LearnScreen}
           options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
+            tabBarLabel: "",
+            tabBarIcon: ({ focused, color, size }) => (
               <Image
                 style={styles.logo}
-                source={settings}
+                source={focused ? settingsPink : settings}
               />
             ),
             tabBarBadge: 3,
@@ -65,11 +70,11 @@ export const Navigate = () => {
           name="Settings"
           component={SettingsScreen}
           options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
+            tabBarLabel: "",
+            tabBarIcon: ({ focused, color, size }) => (
               <Image
                 style={styles.logo}
-                source={settings}
+                source={focused ? settingsPink : settings}
               />
             ),
           }}
@@ -78,11 +83,11 @@ export const Navigate = () => {
           name="Task"
           component={TestBlock}
           options={{
-            tabBarLabel: '',
-            tabBarIcon: ({ color, size }) => (
+            tabBarLabel: "",
+            tabBarIcon: ({ focused, color, size }) => (
               <Image
                 style={styles.logo}
-                source={settings}
+                source={focused ? settingsPink : settings}
               />
             ),
           }}
@@ -93,8 +98,8 @@ export const Navigate = () => {
 };
 
 const styles = StyleSheet.create({
-  logo:{
+  logo: {
     width: 50,
     height: 50,
-  }
+  },
 });
