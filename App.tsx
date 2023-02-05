1e4a2e1a4af43a2ff063b7f60487fda27327af32
Romik1999 <usankin.roma@gmail.com>
2023-02-05 15:16:35 +0400

    work in redux

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import React from "react";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Navigate } from "./Navigate";
+import { Provider } from "react-redux";
+import { store } from "./bll/store";
 
 export const App = () => {
   return (
-    <Navigate/>
+    <Provider store={store}>
+      <Navigate />
+    </Provider>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    backgroundColor: 'red',
-  },
+    backgroundColor: "red"
+  }
 });
